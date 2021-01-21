cf64c049271b5fd26ee6e433b9a34116379f5a9b
Anton io <anton.anatolitch@yandex.ru>
2021-01-21 12:32:46 +0300

    add deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
+  router: {
+    base: '/nuxt-online-shop/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
